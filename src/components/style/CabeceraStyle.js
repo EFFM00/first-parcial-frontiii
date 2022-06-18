91cff5589f9410ebdcf120c635e483952fc382c7
EFFM00 <fischietto.elena@gmail.com>
2022-06-17 23:58:43 -0300

    finish work

--- src/components/style/CabeceraStyle.js
+++ src/components/style/CabeceraStyle.js
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 export const H1 = styled.h1`
     color: white;
     font-family: 'Cabin Condensed', sans-serif;
-    font-size: 2.5rem;
-    padding-bottom: 3rem;
+    font-size: 1.8rem;
+    padding-bottom: 2.5rem;
     letter-spacing: 0.9rem;
     font-weight: 700;
 `;
 
 export const Header = styled.header`
     background-color: #181818;
-    padding: 4rem;
+    padding: 2.5rem;
     text-align: center;
     box-shadow: 0px 5px 20px 0px black;
     margin-bottom: 6rem;
